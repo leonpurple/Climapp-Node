89940d6a1714202cbc205a1143201e3a024219e4
leonpurple <leonpurple.2903@gmail.com>
2022-08-08 10:06:20 +0200

    modificando inquirer

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -12,32 +12,17 @@ const menuOpts = [
         choices: [
             {
                 value: '1',
-                name: `${'1'.red}. Crear tarea`
+                name: `${'1'.green}. Buscar Ciudad`
             },
             {
                 value: '2',
-                name: `${'2'.red}. Listar tareas`
-            },
-            {
-                value: '3',
-                name: `${'3'.red}. Listar tareas completadas`
-            },
-            {
-                value: '4',
-                name: `${'4'.red}. Listar tareas pendientes`
-            },
-            {
-                value: '5',
-                name: `${'5'.red}. Completar tarea(s)`
-            },
-            {
-                value: '6',
-                name: `${'6'.red}. Borrar tarea`
+                name: `${'2'.green}. Historial`
             },
             {
                 value: '0',
-                name: `${'0'.red}. Salir`
+                name: `${'0'.green}. Salir`
             }
+         
         ]
     }
 ]
