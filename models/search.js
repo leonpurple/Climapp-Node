b634cd158fdec0aa3b8d66a2db0d2fa7ab18bbf5
leonpurple <leonpurple.2903@gmail.com>
2022-08-08 20:27:38 +0200

    generando token con la api de geocoding

--- models/search.js
+++ models/search.js
@@ -13,7 +13,7 @@ class Busquedas {
 			
 				//aqui hacemos la peticion http
 				// console.log('ciudad',lugar)
-				const resp = await axios.get('https://reqres.in/api/users?page=2')
+				const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/hurlingham%20buenos.json?limit=5&types=place%2Cpostcode%2Caddress&language=es&access_token=pk.eyJ1IjoiY3J1emNydXoiLCJhIjoiY2w2bDF6Nm55MDI2YjNqcTZpYWVyazJmdSJ9._9Cm1d7Wxd1SYVRSzgmINQ')
 				console.log(resp.data)
 			
 				return [] //returna las ciudades que coinsidan con los lugugares con los q escribe la persona
